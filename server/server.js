0cfa7b4b57f87c14ced6ef78040b817209ff2a3e
Alan Zhao <alan.zhao908@gmail.com>
2019-04-24 10:34:15 -0400

    added new ec2 address

--- server/server.js
+++ server/server.js
@@ -4,9 +4,8 @@ const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 3000;
 
-
-app.use(express.static(path.join(__dirname, '../public')));
 app.use(cors());
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('/:id', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'))
